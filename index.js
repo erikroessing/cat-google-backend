a58746257ce3680944b6e08f0029dbe1f1df6333
Erik Roessing <emroessing@gmail.com>
2022-08-21 13:35:45 -0400

    add origin test 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.get('/', function (req, res) {
     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log(fullUrl)
-    console.log(req.get('origin'))
+    console.log(req.headers.origin)
     // console.log(process.env.TIMES)
     res.send('GET request to homepage');
   })
