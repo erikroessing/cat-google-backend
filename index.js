428c62b61c529d1f14e44cdb800fc76d80ccfd19
Erik Roessing <emroessing@gmail.com>
2022-08-21 13:25:14 -0400

    fix package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
