e763da02b14999148e8fd0193e6d5ab051841eda
Erik Roessing <emroessing@gmail.com>
2022-08-21 13:18:20 -0400

    fix package.json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5001;
 app.get('/', function (req, res) {
     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log(fullUrl)
-    console.log(process.env.TIMES)
+    // console.log(process.env.TIMES)
     res.send('GET request to homepage');
   })
 
