75a6ee6d42f4bc32960690d9df2b08a2972ec7db
Erik Roessing <emroessing@gmail.com>
2022-08-21 13:53:31 -0400

    add origin test 2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log(fullUrl)
-    console.log(req.headers.origin)
+    console.log(req.socket.remoteAddress)
     // console.log(process.env.TIMES)
     res.send('GET request to homepage');
   })
